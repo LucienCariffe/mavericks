169e23d621351f80870f328a548cec78ceab6464
Lucien Cariffe <lucienmcariffe@gmail.com>
2024-02-10 13:20:53 -0600

    added Theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 
 import './App.css';
 import Header from './components/header';
+import { createMuiTheme, ThemeProvider } from '@mui/material';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main:'#0064B1'
+    },
+    secondary:  {
+      main: '#0064B1'
+    }
+  },
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    fontWeightLight: 500,
+    fontWeightRegular: 600,
+    fontWeightMedium: 700,
+    fontWeightBold: 800
+  }
+
+})
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
-      <header className="App-header">
+      <header >
        <Header/>
       </header>
     </div>
+    </ThemeProvider>
   );
 }
 
